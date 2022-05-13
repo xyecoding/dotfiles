d6d0596cccbb8a001642e8f8e8c7d763a93dec77
xyegithub <xye@bupt.edu.cn>
2022-05-13 09:50:15 +0800

    new

--- conf_files/fish/config.fish
+++ conf_files/fish/config.fish
@@ -17,24 +17,22 @@ alias rm trash-put
 set de /mnt/c/Users/12197/Desktop
 set do /mnt/c/Users/12197/Downloads
 alias ctp 'cd ~/temp_try'
-alias cvh 'cd ~/.config/lvim/mylvim'
-alias csn 'cd ~/.config/lvim/mylvim/snippets/vscode/snippets'
-alias cvv 'cd ~/.local/share/lunarvim/lvim/lua/lvim'
+alias cdh 'cd ~/dotfiles'
+alias cvh 'cd ~/.local/share/lunarvim/lvim/'
 alias cbg 'cd ~/myBlog'
 alias cpp 'cd ~/mypaper/Understanding'
 set -U fish_cursor_insert line
 set -U fish_cursor_default block
 
 
+alias uwc '~/dotfiles/shell_scripts/use_curl_wget.fish'
+alias nuwc '~/dotfiles/shell_scripts/no_use_curl_wget.fish'
+
 alias vvc 'vi ~/.config/lvim/config.lua'
-alias vvp 'vi ~/.local/share/lunarvim/lvim/lua/lvim/plugins.lua'
-alias vvi 'vi ~/.local/share/lunarvim/lvim/init.lua'
 alias vfc 'vi ~/dotfiles/conf_files/fish/config.fish'
 alias wda 'fish ~/myDiary/create_diary.fish'
 alias cda 'cd ~/myDiary'
 
-alias wget '~/dotfiles/shell_scripts/wget'
-alias curl '~/dotfiles/shell_scripts/curl'
 alias bk '~/dotfiles/shell_scripts/backup_files.fish'
 alias gp 'grep -n -H -R'
 
