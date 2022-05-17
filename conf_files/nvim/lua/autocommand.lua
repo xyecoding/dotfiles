0c60f77a8bc2c22a811ec3ffbe43c3ee5e2bea62
xyegithub <xye@bupt.edu.cn>
2022-05-17 14:13:48 +0800

    new

--- conf_files/nvim/lua/autocommand.lua
+++ conf_files/nvim/lua/autocommand.lua
@@ -3,17 +3,11 @@ vim.cmd [[
     autocmd!
     autocmd BufRead * autocmd BufWinEnter * ++once normal! zx
   augroup end
-
-augroup remember_folds
-  autocmd!
-  autocmd BufWinLeave *.* mkview
-  autocmd BufWinEnter *.* silent! loadview
-augroup END
 ]]
 -- The vim script command should be write in [[]]
--- lvim.autocommands.custom_groups = {
---   -- Return to last edit position when opening files (You want this!)
---   { "BufReadPost", "*", [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]] },
---   -- { "BufWinLeave", "*", "silent mkview" },
---   -- { "BufWinEnter", "*", "loadview" },
--- }
+lvim.autocommands.custom_groups = {
+  --   -- Return to last edit position when opening files (You want this!)
+  { "BufReadPost", "*", [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]] },
+  --   -- { "BufWinLeave", "*", "silent mkview" },
+  --   -- { "BufWinEnter", "*", "loadview" },
+}
