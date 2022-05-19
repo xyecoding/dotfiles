c17d5a4a9b750b8c3d46fdd6071a03f7e3194920
xyegithub <xye@bupt.edu.cn>
2022-05-19 18:28:10 +0800

    new

--- arch_usr_init/apps/local/formatters.sh
+++ arch_usr_init/apps/local/formatters.sh
@@ -1,11 +1,9 @@
-~/anaconda3/bin/conda install latexindent.pl -c conda-forge
+~/anaconda3/bin/conda install latexindent.pl=3.17.0 -c conda-forge
 mv ~/anaconda3/lib/perl5/vendor_perl/LatexIndent/defaultSettings.yaml ~/anaconda3/lib/perl5/vendor_perl/LatexIndent/defaultSettings.yaml_b
 ln -s ~/dotfiles/conf_files/latexindent/defaultSettings.yaml ~/anaconda3/lib/perl5/vendor_perl/LatexIndent/defaultSettings.yaml
 ln -s ~/anaconda3/bin/latexindent.pl ~/anaconda3/bin/latexindent
 
-sudo apt install golang-go -y
-export GO111MODULE=on
-go env -w GOPROXY=https://goproxy.cn
-go get -u mvdan.cc/sh/cmd/shfmt
+sudo pacman -S go
+go install mvdan.cc/sh/cmd/shfmt@latest
 npm install -g remark-cli
 npm install prettier remark-prettier -g
