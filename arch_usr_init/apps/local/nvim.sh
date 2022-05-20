c9128e62a58914198802f88f5b86d917da6654b4
xyegithub <xye@bupt.edu.cn>
2022-05-20 10:29:39 +0800

    new

--- arch_usr_init/apps/local/nvim.sh
+++ arch_usr_init/apps/local/nvim.sh
@@ -5,5 +5,6 @@ ln -s ~/dotfiles/conf_files/nvim ~/.config/lvim/mylvim
 
 rm ~/.config/lvim/config.lua
 
-ln -s  ~/.config/lvim/mylvim/lua/config.lua ~/.config/lvim/config.lua
+ln -s ~/.config/lvim/mylvim/lua/config.lua ~/.config/lvim/config.lua
 sudo pacman -S xsel
+pip install neovim-remote
--- linux_usr_init/apps/local/nvim.sh
+++ linux_usr_init/apps/local/nvim.sh
@@ -5,4 +5,5 @@ ln -s ~/dotfiles/conf_files/nvim ~/.config/lvim/mylvim
 
 rm ~/.config/lvim/config.lua
 
-ln -s  ~/.config/lvim/mylvim/lua/config.lua ~/.config/lvim/config.lua
+ln -s ~/.config/lvim/mylvim/lua/config.lua ~/.config/lvim/config.lua
+pip install neovim-remote
