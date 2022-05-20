8789a6867007f6b9e108e0d119c6cdf42e1ac1c7
xyegithub <xye@bupt.edu.cn>
2022-05-20 09:36:02 +0800

    new

--- conf_files/fish/config.fish
+++ conf_files/fish/config.fish
@@ -2,6 +2,8 @@ set -x PATH ~/go/bin $PATH
 set -x PATH /usr/local/luarocks/bin $PATH
 set -x PATH /usr/local/texlive/2021/bin/x86_64-linux $PATH
 
+set -x  LC_ALL "en_US.UTF-8"
+set -x WINEARCH "win32"
 set -x PATH ~/.cargo/bin $PATH
 set -x PATH ~/.fnm $PATH
 set -x PATH ~/.local/bin $PATH
