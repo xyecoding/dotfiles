1da5f76ca4cab569aed84adb3e69fdc5d3df969d
xyegithub <xye@bupt.edu.cn>
2023-04-21 16:05:41 +0800

    new

--- conf_files/nvim/lua/autocommand.lua
+++ conf_files/nvim/lua/autocommand.lua
@@ -6,11 +6,23 @@ vim.cmd [[
 
   " augroup vimtex_group
   "   autocmd!
-  "   autocmd FileType tex call vimtex#init()
+  "   autocmd FileType tex vim.cmd 'call vimtex#init()'
   " augroup end
 ]]
 
 vim.api.nvim_create_autocmd("BufReadPost", {
   pattern = { "*" },
   command = [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]],
-})
+}
+)
+
+lvim.autocommands = {
+  {
+    "BufEnter", -- see `:h autocmd-events`
+    {
+      -- this table is passed verbatim as `opts` to `nvim_create_autocmd`
+      pattern = { "*.tex" }, -- see `:h autocmd-events`
+      command = "call vimtex#init()",
+    }
+  },
+}
