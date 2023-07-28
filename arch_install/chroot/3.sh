ec850154958ba669f5b49c63def8b2fc9822bc43
xyegithub <xye@bupt.edu.cn>
2023-07-28 10:57:16 +0800

    new

--- arch_install/chroot/3.sh
+++ arch_install/chroot/3.sh
@@ -1,5 +1,5 @@
 pacstrap /mnt base base-devel linux linux-headers linux-firmware  #base-devel在AUR包的安装是必须的
-pacstrap /mnt dhcpcd iwd vim bash-completion   #一个有线所需(iwd也需要dhcpcd) 一个无线所需 一个编辑器 一个补全工具
+pacstrap /mnt dhcpcd iwd vim bash-completion tmux  #一个有线所需(iwd也需要dhcpcd) 一个无线所需 一个编辑器 一个补全工具
 
 genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
