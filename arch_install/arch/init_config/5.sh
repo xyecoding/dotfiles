1bbe5aeef424758719e10f692c005457f2c2d446
xyegithub <xye@bupt.edu.cn>
2023-07-28 18:47:28 +0800

    new

--- arch_install/arch/init_config/5.sh
+++ arch_install/arch/init_config/5.sh
@@ -4,10 +4,11 @@ sudo pacman -S fcitx5-anthy #日文输入引擎
 yay -S fcitx5-pinyin-moegirl #萌娘百科词库 由于中国大陆政府对github封锁，你可能在此卡住。如卡住，可根据后文设置好代理后再安装
 sudo pacman -S fcitx5-pinyin-zhwiki #中文维基百科词库
 sudo pacman -S fcitx5-material-color #主题
+yay -S ripgrep fzf
 
-# cat >> /etc/environment << 'EOF'
-# GTK_IM_MODULE=fcitx
-# QT_IM_MODULE=fcitx
-# XMODIFIERS=@im=fcitx
-# SDL_IM_MODULE=fcitx
-# EOF
+cat >> /etc/environment << 'EOF'
+GTK_IM_MODULE=fcitx
+QT_IM_MODULE=fcitx
+XMODIFIERS=@im=fcitx
+SDL_IM_MODULE=fcitx
+EOF
