45fcac3c3ff7648d30b317e5180cf8fd287f19e0
xyegithub <xye@bupt.edu.cn>
2023-07-29 17:52:53 +0800

    new

--- suchless/st/config.default.h
+++ suchless/st/config.default.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:lixelsize=50:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font:lixelsize=30:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
