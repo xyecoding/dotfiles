7a14fdafc7e48aecd1be1cb3f6a3c760857a10d9
xyegithub <xye@bupt.edu.cn>
2023-09-11 16:16:12 +0800

    new

--- arch_usr_init/apps/local/nvim.sh
+++ arch_usr_init/apps/local/nvim.sh
@@ -1,6 +1,7 @@
 #bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
-cd ~/.local
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz
-ln -s ~/.local/nvim-linux64/bin/nvim ~/.local/bin
+# cd ~/.local
+# wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+# tar xzvf nvim-linux64.tar.gz
+# ln -s ~/.local/nvim-linux64/bin/nvim ~/.local/bin
+yay -S neovim-nightly-bin
 
